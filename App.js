cd2cc08b3a6cc8294479606961e20b2862e70987
hanjy-slowturtle <h920915@gmail.com>
2021-04-13 01:09:38 +0900

    #1 LOGIC

--- App.js
+++ App.js
@@ -1,25 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Loading from './Loading';
+import * as Location from 'expo-location';
+import { Alert, Platform } from 'react-native';
+import axios from 'axios';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Hello!!!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const API_KEY = '241051bf13976dd3ddf8b8d9f247255e';
+
+export default class extends React.Component {
+  state = {
+    isLoading: true
+  };
+
+  render() {
+    const { isLoading } = this.state;
+    return (
+      isLoading ? <Loading /> : null
+    );
+  }
+
+  componentDidMount() {
+    this.getLocation();
+  }
+
+  getLocation = async () => {
+    try {
+      await Location.requestPermissionsAsync();
+      const { coords: { latitude, longitude} } = await Location.getCurrentPositionAsync();
+      console.log(latitude, longitude);
+      this.getWeather(latitude, longitude);
+      this.setState({ isLoading: false });
+    } catch(error) {
+      console.log(error);
+      myAlert("Can't find you", 'So Sad');
+    }
+  }
+  getWeather = async (latitude, longitude) => {
+    const { data } = await axios.get(`http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=${API_KEY}`);
+    console.log(data);
+    return data;
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    color: 'white',
-    width: '100%'
+function myAlert(title, message) {
+  if(Platform.OS === 'web') {
+    alert(title + '\n' + message);
+  } else {
+    Alert.alert(title, message);
   }
-});
+}

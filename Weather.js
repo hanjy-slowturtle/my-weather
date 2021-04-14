33588ef2df255c3192e2ce9084938aada7f5ffc3
hanjy-slowturtle <h920915@gmail.com>
2021-04-14 23:40:03 +0900

    #2 STYLES 2

--- Weather.js
+++ Weather.js
@@ -1,17 +1,37 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, StatusBar } from 'react-native';
 import PropTypes from 'prop-types';
+import { LinearGradient } from 'expo-linear-gradient';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 
+const weatherOptions = {
+    Clear: {
+        iconName: 'weather-sunset',
+        gradient: ['#4da0b0', '#d39d38']
+    }
+};
+
 export default function Weather({ temp, condition }) {
+    const weatherOption = weatherOptions[condition] || {
+        iconName: 'weather-hail',
+        gradient: ['red', 'blue']
+    };
+
     return (
-        <View style={styles.container}>
+        <LinearGradient
+            colors={weatherOption.gradient}
+            style={styles.container}>
+            <StatusBar barStyle="light-content" />
             <View style={styles.halfContainer}>
-                <MaterialCommunityIcons name="weather-lightning-rainy" size={96} />
+                <MaterialCommunityIcons
+                    name={weatherOption.iconName}
+                    size={96}
+                    color="white"
+                />
                 <Text style={styles.temp}>{temp}°C</Text>
             </View>
             <View style={styles.halfContainer}></View>
-        </View>
+        </LinearGradient>
     );
 }
 
@@ -38,7 +58,8 @@ const styles = StyleSheet.create({
         alignItems: 'center'
     },
     temp: {
-        fontSize: 42
+        fontSize: 42,
+        color: 'white'
     },
     halfContainer: {
         flex: 1,
